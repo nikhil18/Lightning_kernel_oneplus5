3879e9c52b0e94fb7bedc8eb2b72b5480add3a2c
nikhil18 <nikhilgolechha1@gmail.com>
2018-09-17 12:02:41 -0700

    some fixes

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1035,14 +1035,6 @@ void init_rt_schedtune_timer(struct sched_rt_entity *rt_se)
 	rt_se->schedtune_enqueued = false;
 }
 
-static void start_schedtune_timer(struct sched_rt_entity *rt_se)
-{
-	struct hrtimer *timer = &rt_se->schedtune_timer;
-
-	hrtimer_start(timer, ns_to_ktime(RT_SCHEDTUNE_INTERVAL),
-			HRTIMER_MODE_REL_PINNED);
-}
-
 /*
  * Update the current task's runtime statistics. Skip current tasks that
  * are not in our scheduling class.
@@ -1417,8 +1409,7 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!task_current(rq, p) && p->nr_cpus_allowed > 1)
 		enqueue_pushable_task(rq, p);
-(??)
-(??)	schedtune_enqueue_task(p, cpu_of(rq));
+	schedtune_enqueue_task(p, cpu_of(rq));
 }
 
 static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
@@ -1434,7 +1425,7 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	walt_dec_cumulative_runnable_avg(rq, p);
 
 	dequeue_pushable_task(rq, p);
-(??)	schedtune_dequeue_task(p, cpu_of(rq));
+	schedtune_dequeue_task(p, cpu_of(rq));
 }
 
 /*
